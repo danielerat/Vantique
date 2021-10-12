aa8f89ab1cbdca573343f67e5fec83e83a535545
danielerat <davidodo2015@gmail.com>
2021-10-12 09:04:13 +0200

    Configuring the db credentials

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "danielerat");
+define("DB_PASS", "dbpassword");
 define("DB_NAME", "vantique");
