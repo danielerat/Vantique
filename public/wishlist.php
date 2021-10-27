80730cda236e1b7be93c3fb599267cf39fc6b576
danielerat <davidodo2015@gmail.com>
2021-10-27 22:53:02 +0200

    adding A wish list

---
+++ public/wishlist.php
@@ -0,0 +1,27 @@
+<?php
+require_once("../private/initialize.php");
+require_once(PRIVATE_PATH . "/shared/public_header.php");
+
+?>
+
+<!-- Wishlist-Page -->
+<div class="page-wishlist" style="margin-bottom:300px;">
+    <div class="vertical-center">
+        <div class="text-center">
+            <h1>Em
+                <i class="fas fa-child"></i>ty!
+            </h1>
+            <h5>No Products were added to the wishlist.</h5>
+            <div class="redirect-link-wrapper u-s-p-t-25">
+                <a class="redirect-link" href="shop-v1-root-category.html">
+                    <span>Return to Shop</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Wishlist-Page /- -->
+
+<?php
+require_once(PRIVATE_PATH . "/shared/public_footer.php");
+?>
