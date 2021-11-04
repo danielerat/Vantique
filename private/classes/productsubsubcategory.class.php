a2d323b608c0c7c8c3d26e26b9b1c7a63e18d5b0
danielerat <davidodo2015@gmail.com>
2021-11-04 19:20:21 +0200

    Creating The sub sub category Class

---
+++ private/classes/productsubsubcategory.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class ProductSubSubCategory extends ProductSubCategory
+{
+    static protected $table_name = 'subSubCategory';
+    static protected $db_columns = ["id", "parentId", "name", 'addedOn'];
+
+    public $id;
+    public $parentId;
+    public $name;
+    public $addedOn;
+
+    public function __construct($args = [])
+    {
+        $this->parentId = $args['parentId'] ?? '';
+        $this->name = $args['name'] ?? '';
+        $this->addedOn = $args['addedOn'] ?? date('Y-m-d H:i:s');
+    }
+}
